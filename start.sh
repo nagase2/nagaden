ab0a395a776799df7cc8fc9e5b668f154d7858be
naggi <nagase2@gmail.com>
2017-12-29 13:00:57 +0900

    updated logsetting

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+ 
+/usr/bin/python3.4 /home/pi/Desktop/nagase-denki/monitoring.py 
 
-/usr/bin/python3.4 /home/pi/Desktop/nagase-denki/monitoring.py > /tmp/monitoring.log
+#>> /tmp/monitoring.log
 
 
