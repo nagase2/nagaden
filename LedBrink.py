dbde7f38ee9292e99fe0fcc27978424315322a3b
Windows222 <nagase2@gmail.com>
2018-01-27 13:07:19 +0900

    stable version released

--- LedBrink.py
+++ LedBrink.py
@@ -28,7 +28,7 @@ fileHandler = RotatingFileHandler('./log/monitoring.log',maxBytes=1000000,backup
 fileHandler.setLevel(DEBUG)
 fileHandler.setFormatter(formatter)
 streamHandler = StreamHandler()
-streamHBUG)
+streamHandler.setLevel(DEBUG)
 streamHandler.setFormatter(formatter)
 logger.setLevel(DEBUG)
 logger.addHandler(fileHandler)
@@ -36,51 +36,58 @@ logger.addHandler(streamHandler)
                           
 
 # interval in sec production value should be 10
-LOOP_INTERVAL=10
+LOOP_INTERVAL=100000
 
-API_KEY = 'aa62d842819f547e4213edd1b9a19e92' #PRODUCTION
-##API_KEY = '06ed89baf1866d4aeac7f21b84e51636' #TEST ENV
+#API_KEY = 'aa62d842819f547e4213edd1b9a19e92' #PRODUCTION
+API_KEY  = '06ed89baf1866d4aeac7f21b84e51636' #TEST ENV
 
-DEVICE_ID = 'df834a7986e9a52d5d28e46dd97e87df' #PRODUCTION
-##DEVICE_ID = 'bbdbe6fd59dfcebbe34727321b61b565' #TEST ENV
+#DEVICE_ID = 'df834a7986e9a52d5d28e46dd97e87df' #PRODUCTION
+DEVICE_ID = 'bbdbe6fd59dfcebbe34727321b61b565' #TEST ENV
+
 count=0
-
 
 templist = []
 try:
-  from m2x.client import M2XClient
-  client = M2XClient(API_KEY)
-  device = client.device(DEVICE_ID)
 
+  
   while True:
+    from m2x.client import M2XClient
+    client = M2XClient(API_KEY)
+    device = client.device(DEVICE_ID)
     try:
-      temp_stream = device.stream('temperture')
-      humidity_stream = device.stream('humidit')
+      
+      #device = client.create_device(
+      #    name='currenttime',
+      #    description='Store current time ',
+      #    visibility= 'public'
+      #)
+      ##temp_stream = device.stream('temperture')
+      ##humidity_stream = device.stream('humidit')
       #set up GPIO
-      GPIO.setmode(GPIO.BCM)
-      GPIO.setup(10, GPIO.OUT)
+      ##GPIO.setmode(GPIO.BCM)
+      ##GPIO.setup(10, GPIO.OUT)
       
       # Get I2C bus
-      bus = smbus.SMBus(1)
+      ##bus = smbus.SMBus(1)
     
       #turn the LED on
-      GPIO.output(10,1)
+      ##GPIO.output(10,1)
       # HDC1000 address, 0x40(64)
       # Select configuration register, 0x02(02)
       #		0x30(48)	Temperature, Humidity enabled, Resolultion = 14-bits, Heater on
-      bus.write_byte_data(0x40, 0x02, 0x30)
+      #bus.write_byte_data(0x40, 0x02, 0x30)
       
       # HDC1000 address, 0x40(64)
       # Send temp measurement command, 0x00(00)
-      bus.write_byte(0x40, 0x00)
+      #bus.write_byte(0x40, 0x00)
       
-      time.sleep(0.5)
+      ##time.sleep(0.1)
       
       # HDC1000 address, 0x40(64)
       # Read data back, 2 bytes
       # temp MSB, temp LSB
-      data0 = bus.read_byte(0x40)
-      data1 = bus.read_byte(0x40)
+      data0 = 11#bus.read_byte(0x40)
+      data1 = 12#bus.read_byte(0x40)
       
       # Convert the data
       temp = (data0 * 256) + data1
@@ -90,17 +97,17 @@ try:
       
       # HDC1000 address, 0x40(64)
       # Send humidity measurement command, 0x01(01)
-      bus.write_byte(0x40, 0x01)
+      #bus.write_byte(0x40, 0x01)
 
       #turn the LED off
-      GPIO.output(10,0)
-      time.sleep(0.5)
+      ##GPIO.output(10,0)
+      ##time.sleep(0.1)
       
       # HDC1000 address, 0x40(64)
       # Read data back, 2 bytes
       # humidity MSB, humidity LSB
-      data0 = bus.read_byte(0x40)
-      data1 = bus.read_byte(0x40)
+      data0 = 3#bus.read_byte(0x40)
+      data1 = 5#bus.read_byte(0x40)
       
       # Convert the data
       humidity = (data0 * 256) + data1
@@ -111,9 +118,9 @@ try:
       logger.debug ("Relative Humidity : %.2f %%" %humidity)
       logger.debug ("Temperature in Celsius : %.2f C" %cTemp)
 
+      # print "Temperature in Fahrenheit : %.2f F" %fTemp
       count+=1
 
-      '''
       # send a message if count surpase LOOP_INTERVAL
       if (count ) >= LOOP_INTERVAL:
         #turn the LED on
@@ -125,6 +132,7 @@ try:
         #reset count
         count=0
 
+      #'''  
       # specfy how long time supress line message last time this program send 
       LINE_MESSAGE_INTERVAL_IN_SEC = 150
 
@@ -150,10 +158,11 @@ try:
           lastMsgSentTime = datetime.datetime.now()
         else:
           logger.warn("do not send message but temp is diffrent a lot!")                                           
-           
+      
+                                
       logger.debug(len(templist))
       logger.debug(templist)
-      '''
+      #'''
       
     except ConnectionError:     
       GPIO.cleanup()
@@ -162,7 +171,7 @@ try:
     except:
       logger.error('Exception message::',sys.exc_info())
       time.sleep(10)
-   
+
       
 except KeyboardInterrupt:
   logger.error ('program will exit since Ctl+C pressed.')
